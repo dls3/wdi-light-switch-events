0365dabcb9050f318a786ab0c26b89fc1a253258
Doug <doug.steinberg@gmail.com>
2017-07-06 14:36:47 -0400

    Body/text goes light or dark after click

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 // When someone hits the button in the switch, remove the current state class (on/off), and apply the opposite class (on/off)
 
+// Swap the colors of the body's background and text by adding or removing the dark/light class
+
 var lightSwitch = document.querySelector('button');
+var body = document.querySelector('body');
 
 
 lightSwitch.addEventListener('click', function(e) {
   if (lightSwitch.className === 'switch on') {
     lightSwitch.className = 'switch off';
+    body.className = 'dark';
   } else {
     lightSwitch.className = 'switch on';
+    body.className = 'light';
   }
 });
