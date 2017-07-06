5cd44ee508510d32a50ecf61173ecb5b5e6408d4
Doug <doug.steinberg@gmail.com>
2017-07-06 14:31:10 -0400

    Class on/off with click of light

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
+// When someone hits the button in the switch, remove the current state class (on/off), and apply the opposite class (on/off)
+
+var lightSwitch = document.querySelector('button');
 
 
+lightSwitch.addEventListener('click', function(e) {
+  if (lightSwitch.className === 'switch on') {
+    lightSwitch.className = 'switch off';
+  } else {
+    lightSwitch.className = 'switch on';
+  }
+});
