a9424cc357ecf74be271b9dafc3a1ad42b84f96b
Doug <doug.steinberg@gmail.com>
2017-07-06 14:40:58 -0400

    Text changes with light switch

--- js/main.js
+++ js/main.js
@@ -2,16 +2,20 @@
 
 // Swap the colors of the body's background and text by adding or removing the dark/light class
 
+// Lastly, change the text in box to say "It's so bright in here!" when it's on, and "Hey, who turned off the lights?" when it's off
+
 var lightSwitch = document.querySelector('button');
 var body = document.querySelector('body');
-
+var text = document.querySelector('.status');
 
 lightSwitch.addEventListener('click', function(e) {
   if (lightSwitch.className === 'switch on') {
     lightSwitch.className = 'switch off';
     body.className = 'dark';
+    text.innerHTML = "It's so bright in here!";
   } else {
     lightSwitch.className = 'switch on';
     body.className = 'light';
+    text.innerHTML = 'Hey, who turned off the lights?'
   }
 });
